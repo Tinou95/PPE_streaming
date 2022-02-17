69c24aa2dbcbf61f6754a8a7fd90bd55ef7ee2c1
Quentin <72099011+Tinou95@users.noreply.github.com>
2022-02-17 14:19:28 +0100

    bgc

--- js/themesombre.js
+++ js/themesombre.js
@@ -6,11 +6,11 @@ const image_header2 = document.querySelector(".image_header2")
 
 
 image_header2.addEventListener("click", () => {
-    if (image_header2.innerHTML = `<img class="image_header2" src="./asset/sunny.png" alt="">`) {
-        image_header2.innerHTML = `<img class="image_header2" src="./asset/moon.png" alt="">`;
+    if (image_header2.src = src="./asset/sunny.png") {
+        image_header2.src = src="./asset/moon.png"
 
     } else {
-        image_header2.innerHTML = `<img class="image_header2" src="./asset/sunny.png" alt="">`;
+        image_header2.src = src="./asset/sunny.png"
 
     }
    
