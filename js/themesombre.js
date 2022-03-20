53c5941054fb4d68a4614ed6482916af55c5517f
Tinou95 <Quentin95170@hotmail.fr>
2022-03-20 15:53:30 +0100

    js modal

--- js/themesombre.js
+++ js/themesombre.js
@@ -7,6 +7,8 @@ const body = document.querySelector("body");
 const form = document.querySelector("main form");
 const hache2 = document.querySelector("main section h2")
 
+
+
 image_header2.addEventListener("click", () => {
     if (image_header2.src = src="./asset/sunny.png") {
         image_header2.src = src="./asset/moon.png"
@@ -18,6 +20,7 @@ image_header2.addEventListener("click", () => {
     body.classList.toggle("body")
     form.classList.toggle("form")
     hache2.classList.toggle("hache2")
+
    
 });
 
