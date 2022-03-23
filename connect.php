5ae2bf7bbe45ec38e96e4d75d323549e28d00919
Tinou95 <Quentin95170@hotmail.fr>
2022-03-23 01:53:17 +0100

    la Js Avancée

--- connect.php
+++ connect.php
@@ -38,44 +38,5 @@ $bdd = new mysqli('localhost','root','','PPE_straming');
 
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Mon menu web">
-    <link rel="icon" type="image" sizes="32x32" href="asset/disney_plus_icon_156735-removebg-preview.png">
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/header.css">
-	<link rel="stylesheet" href="css/connect.css">
-	<link rel="stylesheet" href="css/footer.css">
-
-    
-    <title>Disney+</title>
-</head>
-<body>
-    <header>
-        <img class="image_header" src="./asset/disney_plus_icon_156735-removebg-preview.png" alt="">
-        <h1>Film, séries TV et bien plus en illimité</h1>
-    </header>
-    <main>
-
-		<section>
-			<h2>Validation &#9989;</h1>
-			<p>Votre formulaire a bien été envoyé</p>
-		</section>
-	
-	</main>
-
-	<footer> 
-        <p>&copy;- Disney+  -2022</p>
-    </footer>
-
-
-    <script src="./js/page2.js"></script>
-</body>
-
-</html>
 
 
--- js/page2.js
+++ js/page2.js
@@ -1,13 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 
+
+    // Redirection vers la premiere page
+
     const image_header = document.querySelector(".image_header");
 
-  
     image_header.addEventListener("click", (e) =>{
       e.preventDefault;
       window.location.href="page1.html"
     })
+
+
+
+    // Formulaire de contact 
+
+    let kcorpWin = document.querySelector("main p");
+    let ville = document.getElementsByTagName("select")[0].value;
+    let pays = document.getElementsByTagName("select")[1].value;
+	  let nom = document.getElementsByTagName("input")[0].value;
+	  let prenom = document.getElementsByTagName("input")[1].value;
+	  let email = document.getElementsByTagName("input")[2].value;
+    let btn = document.getElementsByTagName("input")[3];
+
+
+
+	let formContact = () => {
+    if (nom != "" && prenom != "" && email != "" && ville != "" && pays != "") {
+      kcorpWin.classList.add("reussi");
+			kcorpWin.innerText = "Formulaire bien envoyé";     
+		} else {
+      kcorpWin.classList.add("erreur");
+			kcorpWin.innerText = "Champs incomplets, veuillez tous les remplir";			
+		}
+	};
+
+  btn.addEventListener("click", e => {
+		e.preventDefault();
+		formContact();
+	});
   
     
   
