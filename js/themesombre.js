24452a071f0fae18421cd9f907e5a01ae93a572c
Tinou95 <Quentin95170@hotmail.fr>
2022-03-28 00:14:28 +0200

    La JS theme BRESOM

--- js/themesombre.js
+++ js/themesombre.js
@@ -8,7 +8,7 @@ const form = document.querySelector("main form");
 const hache2 = document.querySelector("main section h2")
 
 
-
+/*
 image_header2.addEventListener("click", () => {
     if (image_header2.src = src="./asset/sunny.png") {
         image_header2.src = src="./asset/moon.png"
@@ -16,13 +16,30 @@ image_header2.addEventListener("click", () => {
     } else {
         image_header2.src = src="./asset/sunny.png"
     }
+});  */
+
+
+image_header2.addEventListener("click", () => {
+
+    //Si la classe est image_css afficher la classe image_css2
+    if (image_header2.classList.contains("image_moon")) {
+        image_header2.classList.remove("image_moon");
+        image_header2.classList.add("image_sunny");
+    } else {
+        image_header2.classList.remove("image_sunny");
+        image_header2.classList.add("image_moon");
+
+    }
 
     body.classList.toggle("body")
     form.classList.toggle("form")
     hache2.classList.toggle("hache2")
-
-   
 });
+
+
+
+
+
 
 
 
