5c897ba8159788fdbf8ddea08937f0d0663a42ae
Tinou95 <Quentin95170@hotmail.fr>
2022-04-09 01:10:47 +0200

    premiere Partie Sport

--- js/page2.js
+++ js/page2.js
@@ -15,35 +15,80 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
+
     // Formulaire de contact 
-    let kcorpWin = document.querySelector(".warning");
-    let ville = document.getElementsByTagName("select")[0].value;
-    let pays = document.getElementsByTagName("select")[1].value;
-	  let nom = document.getElementsByTagName("input")[0].value;
-	  let prenom = document.getElementsByTagName("input")[1].value;
-	  let email = document.getElementsByTagName("input")[2].value;
-    let btn = document.getElementsByTagName("input")[3];
 
+    let btn = document.getElementById("submit");
+    let el = document.querySelector(".warning");
+
+    btn.addEventListener("click", e => {
+      e.preventDefault();
+      leForm();
+    });
+    let leForm = () => {
+let ville = document.getElementsByTagName("select")[0].value;
+let pays = document.getElementsByTagName("select")[1].value;
+let nom = document.getElementsByTagName("input")[0].value;
+let prenom = document.getElementsByTagName("input")[1].value;
+let email = document.getElementsByTagName("input")[2].value;
+
+console.log(nom, prenom, email, ville, pays);
+
+      if (!nom  || !prenom || !email || !ville  || !pays ) {
+        el.innerText = "Veuillez remplir tous les champs ";
+       el.style.backgroundColor = "red";
+
+      } else {
+        el.innerText = "Votre formulaire a bien été envoyé";
+        el.style.backgroundColor = "green";
+
+        localStorage.setItem("user", nom + " | " + prenom + " | " + email + " | " + ville + " | " + pays);
+        sessionStorage.setItem("user", "sessionid");
+  
+      }
+    };
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let kcorpWin = document.querySelector(".warning");
+let ville = document.getElementById("ville_select");
+let pays = document.getElementById("pays_select");
+let nom = document.getElementById("last_name");
+let prenom = document.getElementById("first_name");
+let email = document.getElementById("mail");
+let btn = document.getElementById("submit");
     
-
-
- 
-
 
 	let formContact = () => {
 
-    if(nom === "" || prenom === "" || email === "" || ville === "" || pays === "" ) {
-    
-      kcorpWin.classList.add("erreur");
-      kcorpWin.classList.remove("reussi");
-			kcorpWin.innerText = "Champs incomplets, veuillez tous les remplir";	 
-		} else {
-
-
+    if(nom != "" && prenom != "" && email != "" && ville != "" && pays != "" ) {
 
       kcorpWin.classList.add("reussi");
       kcorpWin.classList.remove("erreur");
-			kcorpWin.innerText = "Formulaire bien envoyé";
+			kcorpWin.innerText = "Formulaire bien envoyé"; 
+      	 
+
+    
+		} else {
+
+      kcorpWin.classList.add("erreur");
+      kcorpWin.classList.remove("reussi");
+			kcorpWin.innerText = "Champs incomplets, veuillez tous les remplir";
       		
 		}
 
@@ -54,6 +99,10 @@ document.addEventListener("DOMContentLoaded", () => {
 		e.preventDefault();
 		formContact();
 	});
+
+  */
+
+  
   
   
     
